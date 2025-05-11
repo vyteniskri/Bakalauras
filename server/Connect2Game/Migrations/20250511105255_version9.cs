36b8c038fc760d6bf23986e341b1f92a893bb48b
vyteniskri <krisciunas.vytenis@gmail.com>
2025-05-11 21:39:39 +0300

    Migrations added

---
+++ server/Connect2Game/Migrations/20250511105255_version9.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Connect2Game.Migrations
+{
+    /// <inheritdoc />
+    public partial class version9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "warnings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CreationDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    Text = table.Column<string>(type: "text", nullable: false),
+                    ForeignKeyReportId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_warnings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_warnings_reports_ForeignKeyReportId",
+                        column: x => x.ForeignKeyReportId,
+                        principalTable: "reports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_warnings_ForeignKeyReportId",
+                table: "warnings",
+                column: "ForeignKeyReportId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "warnings");
+        }
+    }
+}
